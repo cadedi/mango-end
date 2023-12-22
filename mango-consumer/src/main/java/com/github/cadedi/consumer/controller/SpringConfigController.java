f601c6f4cfd3974d2683174c3114b301a5a80324
cadedi <stscyk@outlook.com>
2023-12-22 11:37:19 +0800

    配置客户端配置中心启用refresh,可手动刷新目标配置文件的读取

--- mango-consumer/src/main/java/com/github/cadedi/consumer/controller/SpringConfigController.java
+++ mango-consumer/src/main/java/com/github/cadedi/consumer/controller/SpringConfigController.java
@@ -1,9 +1,14 @@
 package com.github.cadedi.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 开启refresh后可通过访问客户端的post接口http://localhost:8005/actuator/refresh手动触发配置的刷新(引入bus则可以自动刷新)
+ */
+@RefreshScope   //运行客户端通过post方法(/actuator/refresh)触发作用域下spring-cloud-config的refresh(配置中心读取文件的改动刷新)
 @RestController
 public class SpringConfigController {
 
